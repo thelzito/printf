badacf98e784ef5ea6016cc95394e25cfa138a20
Noxolo Nkebe <133243184+Nnkebe@users.noreply.github.com>
2023-07-26 01:13:43 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -55,7 +55,7 @@ int _printf(const char *format, ...)
 /**
  * print_buffer - Prints the contents of the buffer if it exist
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: The index at which to add the next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
